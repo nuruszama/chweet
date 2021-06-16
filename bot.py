f57b7ad31b91cec6a8cf402d77ad357a2c246d89
nuruszama <85991146+nuruszama@users.noreply.github.com>
2021-06-16 18:40:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://chweet.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://chweety.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
