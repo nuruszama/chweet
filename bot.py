81bf8b0c970a94a122fb4caaebb9b60aae627c01
nuruszama <85991146+nuruszama@users.noreply.github.com>
2021-06-16 19:33:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,6 +29,12 @@ def echo(update, context):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
+def pass1(update, context):
+    update.message.reply_text('Hey you send me')
+
+def pass2(update, context):
+    update.message.reply_text('Sorry, but right now I am still learning to respond like humans')
+
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -48,7 +54,9 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, pass1))
     dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, pass2))
 
     # log all errors
     dp.add_error_handler(error)
