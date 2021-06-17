543fc3184c68d2e26525e68d42f2537ed68e4990
nuruszama <85991146+nuruszama@users.noreply.github.com>
2021-06-17 20:14:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ def help(update, context):
 
 def echo(update, context):
     """Echo the user message."""
-    mention = "["+user_name+"](tg://user?id="+str(user_id)+")"
+    """mention = "["+user_name+"](tg://user?id="+str(user_id)+")" """
     """using mention,parse,parse_mode="Markdown" to mention the sender"""
     update.message.reply_text(update.message.text)
 
