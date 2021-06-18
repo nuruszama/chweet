1312cdb30b382c47560a1fd0d4b1dc808c4a87c0
nuruszama <85991146+nuruszama@users.noreply.github.com>
2021-06-18 09:20:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from telegram.ext import (
     Updater,
     CommandHandler,
     MessageHandler,
-    Filters
+    Filters,
     ConversationHandler,
     CallbackContext,
 )
@@ -68,7 +68,18 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, incoming))
+    conv_handler = ConversationHandler(
+        entry_points=[MessageHandler(Filters.text, incoming)],
+        states={
+                ECHO: [
+                MessageHandler(
+                     (MessageHandler(Filters.text, echo)
+                )
+        },
+        fallbacks=[MessageHandler(Filters.text, end)],
+    )
+
+    dp.add_handler(conv_handler)
 
     # log all errors
     dp.add_error_handler(error)
