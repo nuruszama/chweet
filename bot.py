76fd888a31eec24162b48fb827f55ce385f88e2b
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2021-08-06 18:44:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -109,7 +109,10 @@ updater.dispatcher.add_error_handler(error)
 #Start the bot.
 def main():
     # Start the Bot
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://chweetbot.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
