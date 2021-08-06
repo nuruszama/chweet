3e39770a9628e5e3ced5510c4cdb2b53b260b132
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2021-08-06 11:57:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,16 @@
 """
-Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
+Simple Bot to resend to Telegram videos or documents inspired from the python-telegram-bot examples.
 Deployed using heroku.
 """
 
 import logging
-from typing import Dict
 
-from telegram import ReplyKeyboardMarkup, Update, ReplyKeyboardRemove
+from telegram import Update
 from telegram.ext import (
     Updater,
     CommandHandler,
     MessageHandler,
     Filters,
-    ConversationHandler,
     CallbackContext,
 )
 
@@ -26,19 +24,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 TOKEN = '1871713199:AAGEn6U4aD2u0EGlwxsXJmbgTsey-cKoimI'
 
-CHOOSING, TYPING_CHOICE, TYPING_REPLY = range (3)
-
-reply_keyboard = [
-    ['Age', 'Favourite colour'],
-    ['Number of siblings', 'Something else...'],
-    ['Done'],
-]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-
-def facts_to_str(user_data: Dict[str, str]) -> str:
-    """Helper function for formatting the gathered user info."""
-    facts = [f'{key} - {value}' for key, value in user_data.items()]
-    return "\n".join(facts).join(['\n', '\n'])
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
@@ -46,60 +31,15 @@ def facts_to_str(user_data: Dict[str, str]) -> str:
 def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text(
-        "Hi @#{message.from.username}! I am Sweety, the cutest 🐈 in the world."
-        "About what you would like to talk to me"
-        "eg: Name, age, hobbies. Reply anything like this"
-    )
-
-    return CHOOSING
-
-def regular_choice(update: Update, context: CallbackContext) -> int:
-    """Ask the user for info about the selected predefined choice."""
-    text = update.message.text
-    context.user_data['choice'] = text
-    update.message.reply_text(f'Your {text.lower()}? Yes, I would love to hear about that!')
-
-    return TYPING_REPLY
-
-def custom_choice(update: Update, context: CallbackContext) -> int:
-    """Ask the user for a description of a custom category."""
+        "Hi @#{message.from.username}! I am Sweety, the cutest 🐈 in the world.")
     update.message.reply_text(
-        "Alright, please send me the category about which you wish to talk"
-        "for example 'Most impressive skill'"
-        )
+        "I will assist you to send documents and videos without forward tag")
 
-    return TYPING_CHOICE
-
-def received_information(update: Update, context: CallbackContext) -> int:
-    """Store info provided by user and ask for the next category."""
-    user_data = context.user_data
-    text = update.message.text
-    category = user_data['choice']
-    user_data[category] = text
-    del user_data['choice']
-
-    update.message.reply_text(
-        "Neat! Just so you know, this is what you already told me:"
-        f"{facts_to_str(user_data)} You can tell me more, or change your opinion"
-        " on something.",
-        reply_markup=markup,
-    )
-
-    return CHOOSING
-
-def done(update: Update, context: CallbackContext) -> int:
-    """Display the gathered info and end the conversation."""
-    user_data = context.user_data
-    if 'choice' in user_data:
-        del user_data['choice']
-
-    update.message.reply_text(
-        f"I learned these facts about you: {facts_to_str(user_data)}Until next time!",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-    user_data.clear()
-    return ConversationHandler.END
+def docs(update, context):
+    """fetching file_id and resending to destination"""
+    If update.message.
+    context.bot.send_document(chat_id=update.message.chat_id,
+                               document=update.message.file_id)
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -115,35 +55,9 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-def done(update: Update, context: CallbackContext) -> int:
-    """Display the gathered info and end the conversation."""
-    user_data = context.user_data
-    if 'choice' in user_data:
-        del user_data['choice']
-
-    update.message.reply_text(
-        f"I learned these facts about you: {facts_to_str(user_data)}Until next time!",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-    user_data.clear()
-    return ConversationHandler.END
-
-    # on conversation
-    conv_handler = ConversationHandler(entry_point, fallbacks)
-    entry_points=[CommandHandler(('start'), start)],
-    states={
-       CHOOSING: [
-           MessageHandler(Filters.regex('^(Age|Favourite colour|Number of siblings)$'), regular_choice),
-           MessageHandler(Filters.regex('^Something else...$'), custom_choice) ],
-       TYPING_CHOICE: [
-           MessageHandler(Filters.text & ~(Filters.command | Filters.regex('^Done$')), regular_choice) ],
-       TYPING_REPLY: [
-           MessageHandler(Filters.text & ~(Filters.command | Filters.regex('^Done$')), received_information) ],
-       },
-    fallbacks=MessageHandler(Filters.regex('^Done$'), done)
-        
-    dp.add_handler(conv_handler)
+    # command handlers
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(**, docs))
 
     # log all errors
     dp.add_error_handler(error)
